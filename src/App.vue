8023f3f89e2cdc2b540ba9d17ea09e70733befdc
zhoulei <403907871@qq.com>
2021-04-23 15:21:18 +0800

    - do dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
 
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个',
+    avatar: 'https://avatars.githubusercontent.com/u/29775873?v=4'
+  }, {
+    id: 2,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个'
+  },
+  {
+    id: 3,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个',
+    avatar: 'https://avatars.githubusercontent.com/u/29775873?v=4'
+  }, {
+    id: 4,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个',
+    avatar: 'https://avatars.githubusercontent.com/u/29775873?v=4'
+  }, {
+    id: 5,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个',
+    avatar: 'https://avatars.githubusercontent.com/u/29775873?v=4'
+  }, {
+    id: 6,
+    title: 'test1专栏',
+    description: '这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个这是一个',
+    avatar: 'https://avatars.githubusercontent.com/u/29775873?v=4'
+  }
+]
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: '周磊',
+  id: 1
+}
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    GlobalHeader,
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData,
+      user: currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
